9c24af4daaa50c8d1d6da380c4a77d111503ee1e
Quba <144456270+Quba456555@users.noreply.github.com>
2025-02-20 16:29:41 +0100

    Update A4.jsx

--- src/components/A4.jsx
+++ src/components/A4.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function A4() {
+function LicznikZObrazkami() {
+  const [stan, ustawStan] = useState(0);
+
+  const zwiekszStan = () => {
+    ustawStan(stan + 1);
+  };
+
+  const zmniejszStan = () => {
+    ustawStan(stan - 1);
+  };
+
+  const wybierzObrazek = () => {
+    if (stan < 10) {
+      return "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg";
+    } else if (stan < 20) {
+      return "https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Ukraine.svg";
+    } else {
+      return "https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Russia.svg";
+    }
+  };
+
   return (
     <div>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg"
-        alt=""
-      />
+      <p>Stan licznika: {stan}</p>
+      <button onClick={zwiekszStan}>Zwiększ licznik</button>
+      <button onClick={zmniejszStan}>Zmniejsz licznik</button>
+      <div>
+        <img src={wybierzObrazek()} alt="Obrazek" />
+      </div>
     </div>
   );
 }
 
-export default A4;
+export default LicznikZObrazkami;
